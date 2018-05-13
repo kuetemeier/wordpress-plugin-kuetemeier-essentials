a8aaac8ea608901f29937fbaafe46f311bfdba05
Joerg Kuetemeier <jk@kuetemeier.net>
2018-05-13 22:10:24 +0200

    Add banner assets, version to build file and code formatting.

--- gulpfile.js
+++ gulpfile.js
@@ -72,8 +72,8 @@ var paths = {
     dest: 'assets/scripts/'
   },
   scripts_public: {
-//    src: ['assets-src/scripts/public/**/*.js','node_modules/imgix.js/dist/imgix.min.js'],
-    src: ['assets-src/scripts/public/**/*.js'],
+    //    src: ['assets-src/scripts/public/**/*.js','node_modules/imgix.js/dist/imgix.min.js'],
+    src: 'assets-src/scripts/public/**/*.js',
     dest: 'assets/scripts/'
   },
   scripts_copy: {
@@ -81,19 +81,19 @@ var paths = {
     dest: 'assets/scripts/'
   },
   images: {
-  	src: 'assets-src/images/**/*',
-  	dest: 'assets/images/'
+    src: 'assets-src/images/**/*',
+    dest: 'assets/images/'
   }
 };
 
 
-gulp.task('clean', function() {
-	return del([ 'assets', 'phpdocs' ]);
+gulp.task('clean', function () {
+  return del(['assets', 'phpdocs']);
 });
 
 
-gulp.task('replace_readme_txt', function() {
-  return gulp.src(["./readme.txt"], {base: './'})
+gulp.task('replace_readme_txt', function () {
+  return gulp.src(["./readme.txt"], { base: './' })
     .pipe(replace(/(Tags: )(.*)/, '$1' + pkg.wordpress.tags))
     .pipe(replace(/(Requires at least: )(.*)/, '$1' + pkg.wordpress.requires_at_least))
     .pipe(replace(/(Tested up to: )(.*)/, '$1' + pkg.wordpress.tested_up_to))
@@ -101,8 +101,8 @@ gulp.task('replace_readme_txt', function() {
 });
 
 
-gulp.task('replace_kuetemeier_essentials_php', function() {
-  return gulp.src(["./kuetemeier-essentials.php"], {base: './'})
+gulp.task('replace_kuetemeier_essentials_php', function () {
+  return gulp.src(["./kuetemeier-essentials.php"], { base: './' })
     .pipe(replace(/([v,V]ersion: )(.*)/, '$1' + pkg.version))
     .pipe(replace(/(Description: )(.*)/, '$1' + pkg.description))
     .pipe(replace(/('KUETEMEIER_ESSENTIALS_VERSION', ')(.*)(')/, '$1' + pkg.version + '$3'))
@@ -111,8 +111,8 @@ gulp.task('replace_kuetemeier_essentials_php', function() {
 });
 
 
-gulp.task('replace_readme_md', function() {
-  return gulp.src(["./readme.md"], {base: './'})
+gulp.task('replace_readme_md', function () {
+  return gulp.src(["./readme.md"], { base: './' })
     .pipe(replace(/(Description: )(.*)/, '$1' + pkg.description))
     .pipe(replace(/([v,V]ersion: )(.*)/, '$1' + pkg.version))
     .pipe(replace(/(Latest stable version: )(.*)/, '$1' + pkg.version_stable))
@@ -123,8 +123,8 @@ gulp.task('replace_readme_md', function() {
 });
 
 
-gulp.task('replace_src_config_php', function() {
-  return gulp.src(["./src/config.php"], {base: './'})
+gulp.task('replace_src_config_php', function () {
+  return gulp.src(["./src/config.php"], { base: './' })
     .pipe(replace(/(PLUGIN_VERSION = ')(.*)(')/, '$1' + pkg.version + '$3'))
     .pipe(replace(/(PLUGIN_VERSION_STABLE = ')(.*)(')/, '$1' + pkg.version_stable + '$3'))
     .pipe(gulp.dest('./'));
@@ -133,14 +133,16 @@ gulp.task('replace_src_config_php', function() {
 
 gulp.task('replace', ['replace_readme_txt', 'replace_kuetemeier_essentials_php', 'replace_readme_md', 'replace_src_config_php']);
 
+
 gulp.task('pot', function () {
-	return gulp.src(['src/**/*.php', './index.php'])
-			.pipe(wpPot( {
-					domain: 'kuetemeier-essentials',
-					package: 'Kuetemeier-Essentials'
-			} ))
-			.pipe(gulp.dest('languages/kuetemeier-essentials.pot'));
+  return gulp.src(['src/**/*.php', './index.php'])
+    .pipe(wpPot({
+      domain: 'kuetemeier-essentials',
+      package: 'Kuetemeier-Essentials'
+    }))
+    .pipe(gulp.dest('languages/kuetemeier-essentials.pot'));
 });
+
 
 gulp.task('gettext', () => {
   gulp.src('language/*.po')
@@ -148,33 +150,34 @@ gulp.task('gettext', () => {
     .pipe(gulp.dest('language'));
 });
 
+
 gulp.task('zip', function () {
   return gulp.src([
-	'./{src,assets,languages}/**/*',
-	'./index.php',
-	'./LICENSE.txt',
-	'./readme.txt',
-	'./uninstall.php',
-	'./kuetemeier-essentials.php',
-	'!./languages/*backup*',
-	'./vendor/{composer,kuetemeier}/**/*',
-	'./vendor/autoload.php',
-	'!./vendor/**/node_modules',
-	'!./vendor/**/node_modules/**/*',
-	'!./vendor/**/.*',
-	'!./vendor/**/composer.lock',
-	'!./vendor/**/gulpfile.js',
-	'!./vendor/**/phpcs.xml*',
-	'!./vendor/**/phpunit.xml*',
-	'!./vendor/**/tests/**/*',
-	'!./vendor/**/tests'
-	], { base: '..' })
-	.pipe(zip('kuetemeier-essentials.zip'))
-	.pipe(gulp.dest('./release/'));
+    './{src,assets,languages}/**/*',
+    './index.php',
+    './LICENSE.txt',
+    './readme.txt',
+    './uninstall.php',
+    './kuetemeier-essentials.php',
+    '!./languages/*backup*',
+    './vendor/{composer,kuetemeier}/**/*',
+    './vendor/autoload.php',
+    '!./vendor/**/node_modules',
+    '!./vendor/**/node_modules/**/*',
+    '!./vendor/**/.*',
+    '!./vendor/**/composer.lock',
+    '!./vendor/**/gulpfile.js',
+    '!./vendor/**/phpcs.xml*',
+    '!./vendor/**/phpunit.xml*',
+    '!./vendor/**/tests/**/*',
+    '!./vendor/**/tests'
+  ], { base: '..' })
+    .pipe(zip('kuetemeier-essentials-v'+pkg.version+'.zip'))
+    .pipe(gulp.dest('./release/'));
 });
 
 
-gulp.task('scripts-admin', function() {
+gulp.task('scripts-admin', function () {
   return gulp.src(paths.scripts_admin.src, { sourcemaps: scripts_sourcemaps })
     .pipe(gulpif(scripts_sourcemaps, sourcemaps.init()))
     .pipe(babel())
@@ -185,7 +188,7 @@ gulp.task('scripts-admin', function() {
 });
 
 
-gulp.task('scripts-public', function() {
+gulp.task('scripts-public', function () {
   return gulp.src(paths.scripts_public.src, { sourcemaps: scripts_sourcemaps })
     .pipe(gulpif(scripts_sourcemaps, sourcemaps.init()))
     .pipe(babel())
@@ -195,15 +198,16 @@ gulp.task('scripts-public', function() {
     .pipe(gulp.dest(paths.scripts_public.dest));
 });
 
-gulp.task('scripts-copy', function() {
+gulp.task('scripts-copy', function () {
   return gulp.src(paths.scripts_copy.src)
     .pipe(gulp.dest(paths.scripts_copy.dest));
 });
 
+
 gulp.task('scripts', ['scripts-public', 'scripts-admin', 'scripts-copy']);
 
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
@@ -215,19 +219,25 @@ gulp.task('styles', function() {
 });
 
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(paths.images.src)
-  	.pipe(gulp.dest(paths.images.dest));
-  });
-
-
-gulp.task('assets-index', function() {
-  return gulp.src('./assets-src/index.php')
-  	.pipe(gulp.dest('./assets/'));
+    .pipe(gulp.dest(paths.images.dest));
 });
 
 
-gulp.task('sass-lint', function() {
+gulp.task('assets-index', function () {
+  return gulp.src('./assets-src/index.php')
+    .pipe(gulp.dest('./assets/'));
+});
+
+
+gulp.task('assets-banner', function () {
+  return gulp.src('./assets-src/*.{png,jpg,svg}')
+    .pipe(gulp.dest('./assets/'));
+});
+
+
+gulp.task('sass-lint', function () {
   return gulp.src(paths.styles.src)
     .pipe(sassLint())
     .pipe(sassLint.format())
@@ -236,25 +246,25 @@ gulp.task('sass-lint', function() {
 
 
 gulp.task('es-lint', () => {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-    return gulp.src(['./gulpfile.js', paths.scripts_admin.src, paths.scripts_public.src, '!node_modules/**'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(esLint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(esLint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(esLint.failAfterError());
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['./gulpfile.js', paths.scripts_admin.src, paths.scripts_public.src, '!node_modules/**'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(esLint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(esLint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(esLint.failAfterError());
 });
 
 
-gulp.task('phpdoc', function() {
-	  exec('bin/generate-phpdocs', function (err, stdout, stderr) {
+gulp.task('phpdoc', function () {
+  exec('bin/generate-phpdocs', function (err, stdout, stderr) {
     //console.log(stdout);
     console.log(stderr);
     //cb(err);
@@ -264,41 +274,41 @@ gulp.task('phpdoc', function() {
 
 gulp.task('pre-watch', gulpSequence('build-without-release', 'phpdoc'));
 
-gulp.task('watch', ['pre-watch'], function() {
+
+gulp.task('watch', ['pre-watch'], function () {
   scripts_sourcemaps = true;
   //gulp.start(gulpSequence('build-without-release', 'phpdoc'));
   //gulp.start('phpdoc');
 
   gulp.watch(paths.styles.src, ['styles']);
-
   gulp.watch(paths.scripts_admin.src, ['scripts-admin']);
   gulp.watch(paths.scripts_public.src, ['scripts-public']);
-
-  gulp.watch(paths.images.src ['images']);
-
+  gulp.watch(paths.images.src['images']);
   gulp.watch([
-  	'./package.json'
-  ], function() {
-	pkg = JSON.parse(fs.readFileSync('./package.json'));
-	gulp.start('replace');
+    './package.json'
+  ], function () {
+    pkg = JSON.parse(fs.readFileSync('./package.json'));
+    gulp.start('replace');
   });
-
   gulp.watch(['./kuetemeier-essentials.php', './src/**/*.php'], ['phpdoc']);
 });
 
-gulp.task('github', function(){
-	gulp.src('./release/kuetemeier-essentials.zip')
-	.pipe(release({
-		token: process.env.GITHUB_TOKEN,
-		tag: 'v' + pkg.version,
-		name: pkg.name + ' v' + pkg.version,
-		body: 'New ' + pkg.name + ' release v' + pkg.version + '!',
-		assetName: 'kuetemeier-essentials.zip',
-		manifest: pkg
-	}));
+
+gulp.task('github', function () {
+  gulp.src('./release/kuetemeier-essentials.zip')
+    .pipe(release({
+      token: process.env.GITHUB_TOKEN,
+      tag: 'v' + pkg.version,
+      name: pkg.name + ' v' + pkg.version,
+      body: 'New ' + pkg.name + ' release v' + pkg.version + '!',
+      assetName: 'kuetemeier-essentials-v'+pkg.version+'.zip',
+      manifest: pkg
+    }));
 });
 
+
 gulp.task('release', ['build', 'github']);
+
 
 gulp.task('test', ['lint']);
 
@@ -306,13 +316,12 @@ gulp.task('test', ['lint']);
 gulp.task('lint', ['sass-lint', 'es-lint']);
 
 
-gulp.task('assets', ['styles', 'scripts', 'images', 'assets-index']);
+gulp.task('assets', ['styles', 'scripts', 'images', 'assets-index', 'assets-banner']);
 
 
 gulp.task('build-without-release', gulpSequence('replace', 'assets'));
 
 
-//gulp.task('build', gulpSequence('clean', 'lint', 'build-without-release', 'zip'));
 gulp.task('build', gulpSequence('clean', 'build-without-release', 'zip'));
 
 

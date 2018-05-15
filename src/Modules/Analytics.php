d484109c2e89d83bde90c216904843e8abda46ff
Joerg Kuetemeier <jk@kuetemeier.net>
2018-05-15 17:18:01 +0200

    Add template code.

--- src/Modules/Analytics.php
+++ src/Modules/Analytics.php
@@ -210,4 +210,24 @@ final class Analytics extends \Kuetemeier\WordPress\PluginModule
         </script>
         <?php
     }
+
+    public function eRecht24Code()
+    {
+        /*
+        ?>
+        <script type="text/javascript">
+        var gaProperty = 'UA-XXXXXXX-X';
+        var disableStr = 'ga-disable-' + gaProperty;
+        if (document.cookie.indexOf(disableStr + '=true') > -1) {
+        window[disableStr] = true;
+        }
+        function gaOptout() {
+        document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+        window[disableStr] = true;
+        alert('Das Tracking durch Google Analytics wurde in Ihrem Browser für diese Website deaktiviert.');
+        }
+        </script>
+        <?php
+        */
+    }
 }
